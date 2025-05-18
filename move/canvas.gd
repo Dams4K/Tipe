c88298d111ffe2a3a631d8a5af3374cdfaf6c711
Dams4K <dams4k@users.noreply.github.com>
2025-05-18 19:29:35 +0200

    pb de la goutte qui remonte la pente car on compare que les pixels droit et bas pour calculer le gradient

--- move/canvas.gd
+++ move/canvas.gd
@@ -9,21 +9,23 @@ func _ready() -> void:
 	var movements_image = Image.create(texture.get_width(), texture.get_height(), false, Image.FORMAT_RGBA8)
 	var movements_image_texture: ImageTexture = ImageTexture.create_from_image(movements_image)
 	
-	Droplet.movements_texture = movements_image_texture
+	Droplet.movements_image = movements_image_texture.get_image()
+	Droplet.image = texture.get_image()
 	movements.texture = movements_image_texture
 	
 	timer.timeout.connect(update)
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.pressed:
-		var droplet = Droplet.new(get_local_mouse_position(), texture)
+		var droplet = Droplet.new(get_local_mouse_position())
 		droplets.append(droplet)
 
 func update():
 	var droplets_alive: Array[Droplet] = []
 	for droplet: Droplet in droplets:
-		var is_alive = droplet.move()
+		var is_alive = droplet.update()
 		if is_alive:
 			droplets_alive.append(droplet)
 	
-	movements.texture = Droplet.movements_texture
+	movements.texture = ImageTexture.create_from_image(Droplet.movements_image)
+	texture = ImageTexture.create_from_image(Droplet.image)
--- move/droplet.gd
+++ move/droplet.gd
@@ -1,29 +1,27 @@
 extends Object
 class_name Droplet
 
-static var movements_texture: Texture2D
+static var movements_image: Image
+static var image: Image
 
 var position: Vector2 = Vector2.ZERO
-var texture: Texture2D = null
 
-func _init(position: Vector2, texture: Texture2D):
+var sediment: float = 0.0
+
+func _init(position: Vector2):
 	self.position = position
-	self.texture = texture
 
 func move() -> bool:
-	var mvt_img = movements_texture.get_image()
-	mvt_img.set_pixelv(position, Color(.0, .0, .0 ,.0))
+	movements_image.set_pixelv(position, Color(.0, .0, .0 ,.0))
 	
 	var is_alive = false
 	
 	if not out_of_bounds():
 		is_alive = true
-		var img = texture.get_image()
-		
-		var Pxy : float =  img.get_pixelv(position).r
-		var Px1y: float =  img.get_pixelv(position + Vector2.RIGHT).r
-		var Pxy1: float =  img.get_pixelv(position + Vector2.DOWN).r
-		var Px1y1: float = img.get_pixelv(position + Vector2.ONE).r
+		var Pxy : float =  image.get_pixelv(position).r
+		var Px1y: float =  image.get_pixelv(position + Vector2.RIGHT).r
+		var Pxy1: float =  image.get_pixelv(position + Vector2.DOWN).r
+		var Px1y1: float = image.get_pixelv(position + Vector2.ONE).r
 		
 		# u = uv.x
 		# v = uv.y
@@ -35,12 +33,37 @@ func move() -> bool:
 		)
 		
 		var dir_new := -g
-		position += dir_new.normalized()
+		position += dir_new / dir_new.length()
 	
-		mvt_img.set_pixelv(position, Color.GREEN)
+		movements_image.set_pixelv(position, Color.GREEN)
 	
-	movements_texture = ImageTexture.create_from_image(mvt_img)
 	return is_alive
 
+func update():
+	var old_position = position
+	var old_height = image.get_pixelv(old_position).r
+	move()
+	var new_height = image.get_pixelv(position).r
+	var delta_height = new_height - old_height
+	
+	if delta_height > 0.0:
+		print("depose")
+		depose(0.0, old_position)
+	else:
+		erode(0.05, old_position)
+
+func erode(amount: float, old_position: Vector2):
+	var previous_amount = image.get_pixelv(old_position).r
+	var eroded_amount = max(0.0, previous_amount - amount)
+	
+	image.set_pixelv(old_position, Color(eroded_amount, eroded_amount, eroded_amount))
+
+func depose(amount: float, old_position: Vector2):
+	var previous_amount = image.get_pixelv(old_position).r
+	var deposed_amount = min(1.0, previous_amount + amount)
+	
+	image.set_pixelv(old_position, Color(deposed_amount, deposed_amount, deposed_amount))
+
+
 func out_of_bounds() -> bool:
-	return position.x < 0 or position.y < 0 or position.x >= texture.get_width()-1 or position.y >= texture.get_height()-1
+	return position.x < 0 or position.y < 0 or position.x >= image.get_width()-1 or position.y >= image.get_height()-1
